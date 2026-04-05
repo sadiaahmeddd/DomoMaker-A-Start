7259801ba1565a84781b7916357e2f7b0cc44732
ahme0543 <ahme0543@algonquinlive.com>
2026-04-05 16:57:33 -0400

    app-router setup

---
+++ server/router.js
@@ -0,0 +1,22 @@
+const controllers = require('./controllers');
+
+const router = (app) => {
+  // Login routes
+  app.get('/login', controllers.Account.loginPage);
+  app.post('/login', controllers.Account.login);
+
+  // Signup routes
+  app.get('/signup', controllers.Account.signupPage);
+  app.post('/signup', controllers.Account.signup);
+
+  // Logout
+  app.get('/logout', controllers.Account.logout);
+
+  // Main app page
+  app.get('/maker', controllers.Domo.makerPage);
+
+  // Default route
+  app.get('/', controllers.Account.loginPage);
+};
+
+module.exports = router;
